09a14910e55c3e2f0b923410b25061a5fa5cc4fe
Yinghui Yang <yinghui.yang@vanderbilt.edu>
2018-03-17 16:51:28 -0500

    fix weight initialziation

--- DeepLearning.py
+++ DeepLearning.py
@@ -5,7 +5,7 @@ import numpy as np
 def weight_initialization(layer_dimension):
     w = []
     for i in range(1, len(layer_dimension)):
-        w.append(np.random.normal(0, 0.05, [layer_dimension[i], layer_dimension[i-1]]))
+        w.append(np.random.rand(layer_dimension[i], layer_dimension[i-1])/5)
 
     return w
 
@@ -34,14 +34,14 @@ def forward_propagation(w, b, x, hidden_activation, output_activation):
 
     layers = len(w)
 
-    z.append(w[0].dot(x) + b[0])
+    z.append(np.dot(w[0],x) + b[0])
     a.append(activation(hidden_activation, z[0]))
 
     for i in range(1,layers-1):
-        z.append(w[i].dot(a[i-1])+b[i])
+        z.append(np.dot(w[i],a[i-1])+b[i])
         a.append(activation(hidden_activation, z[i]))
 
-    z.append(w[layers-1].dot(a[layers-2])+b[layers-1])
+    z.append(np.dot(w[layers-1],a[layers-2])+b[layers-1])
     a.append(activation(output_activation,z[layers-1]))
     return [a, z]
 
@@ -63,23 +63,26 @@ def backward_propagation(w, b, a, z, x, y, alpha, hidden_activation):
     dz = []
 
     layers = len(w)
+    m = len(y)
 
     for i in range(layers):
-        dw.append(np.copy(w[i]))
-        db.append(np.copy(b[i]))
-        dz.append(np.copy(z[i]))
+        dw.append(np.zeros(np.shape(w[i])))
+        db.append(np.zeros(np.shape(b[i])))
+        dz.append(np.zeros(np.shape(z[i])))
 
     dz[layers-1] = a[layers-1] - y
-    dw[layers-1] = dz[layers-1].dot(a[layers - 2].T) / (np.shape(dz[layers-1]))[1]
-    db[layers-1] = np.sum(dz[layers-1], axis=1) / (np.shape(dz[layers-1]))[1]
+    dw[layers-1] = np.dot(dz[layers-1],a[layers - 2].T)/m
+    db[layers-1] = np.sum(dz[layers-1], axis=1)/m
+
     for i in range(layers-2, 0, -1):
-        dz[i] = w[i+1].T.dot(dz[i+1])*derivative(hidden_activation,z[i])
-        dw[i] = dz[i].dot(a[i-1].T) / (np.shape(dz[i]))[1]
-        db[i] = np.sum(dz[i], axis=1) / (np.shape(dz[i]))[1]
+        dz[i] = np.dot(w[i+1].T,dz[i+1])*derivative(hidden_activation,z[i])
+        dw[i] = np.dot(dz[i],a[i-1].T)/m
+        db[i] = np.sum(dz[i], axis=1)/m
 
     dz[0] = w[1].T.dot(dz[1])*derivative(hidden_activation, z[0])
-    dw[0] = dz[0].dot(x.T) / (np.shape(dz[0]))[1]
-    db[0] =  np.sum(dz[0], axis=1) / (np.shape(dz[0]))[1]
+    dw[0] = np.dot(dz[0],x.T)/m
+    db[0] =  np.sum(dz[0], axis=1)/m
+
     for j in range(layers):
         w[j] = w[j] - alpha * dw[j]
         # db[j] is 1d array, transpose b[j] so the broadcast is happy
